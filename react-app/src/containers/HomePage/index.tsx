d1d2087b81c4801631e3dd9607fa6d502967f93b
ecooper45 <ecooper45@gatech.edu>
2022-09-11 18:38:05 -0400

    Fixed crazy layout of homepage and added real buttons

--- react-app/src/containers/HomePage/index.tsx
+++ react-app/src/containers/HomePage/index.tsx
@@ -30,27 +30,9 @@ class HomePage extends Component {
                             </Box>
                     </div>
                 </div>
-                
 
-            </div>
-            /*<body>
-                <div>
-                    <div className="header">
-
-                        <div className="profile_box">
-                            <p className="user_icon"> </p>
-                            <p className="username">George Burdell</p>
-                            <p className="arrow_icon"> </p>
-                        </div>
-
-                        <div className="help_icon"></div>
-                        <div className="search_box">
-                            <input type="text" placeholder="Search for a client..." name="search" className="search_text"/>
-                            <p className="search_icon"></p>
-                        </div>
-                    </div>
-
-                    <div className="main_content">
+                <div className="main_content">
+                    <div className="top_buttons">
                         <Button variant="contained" onClick={() => {
                             // TODO: Handle Create Team click here
                             console.log('create team clicked')
@@ -59,96 +41,67 @@ class HomePage extends Component {
                             Create Team
                         </Button>
 
-                    
-                    <div className="create_team_plus"></div>
+                        <Button variant="contained" onClick={() => {
+                            // TODO: Handle import from excel click here
+                            console.log('import from excel clicked')
+                        }}
+                        >
+                            Import From Excel
+                        </Button>
+                        
+                        {/* TODO: Filtering goes here! */}
+                    </div>
 
-                    <div className="import_box">
-                        <p className="import_text"> Import from Excel</p>
-                        </div>
-                    <div className="import_plus"></div>
+                    <div className="grid">
+                        {/* TODO: DataGrid here? Seems perfect for our needs but not sure if it is available for us */}
+                        <Box sx={{
+                            width: 1500,
+                            height: 500,
+                            backgroundColor: 'primary.main'
 
-                    <div className="copy_emails_button">
-                        <p className="copy_emails_text">Copy Emails to Clipboard</p>
-                        </div>
+                        }} />
+                    </div>
 
-                    <div className="export_button">
-                        <p className="export_text">Export to Excel</p>
-                        </div>
+                    <div className="bottom_buttons">
+                        
+                        <div className="bottom_buttons_group">
+                            <Button variant="contained" onClick={() => {
+                                // TODO: Handle click here
+                                console.log('copy emails clicked')
+                            }}
+                            >
+                                Copy Emails to Clipboard
+                            </Button>
 
-                    <div className="manage_button">
-                        <p className="manage_text">Manage Selected Team</p>
-                        </div>
-
-                    <div className="delete_button">
-                        <p className="manage_text">Delete Selected Team(s)</p>
-                        </div>
-
-
-
-
-                    <div className="grid_back"></div>
-                    <div className="grid_top_left"></div>
-                    <div className="team_num">
-                        <p className="grid_header_text">Team Number</p>
-                        </div>
-                    <div className="section">
-                            <p className="grid_header_text">Section</p>
-
-                        </div>
-                        <div className="project">
-                            <p className="grid_header_text">Project</p>
-                        </div>
-                        <div className="client">
-                            <p className="grid_header_text">Client</p>
-                        </div>
-                        <div className="professor">
-                            <p className="grid_header_text">Professor</p>
+                            <Button variant="contained" onClick={() => {
+                                // TODO: Handle click here
+                                console.log('export to excel clicked')
+                            }}
+                            >
+                                Export to Excel
+                            </Button>
                         </div>
 
-                        <div className="grid_header_border"></div>
-
-                        <div className="entries">
-                            <div className="light_entry">
-                                <p className="checkbox_entry"> _</p>
-                                <p className="team_num_entry">2100</p>
-                                <p className="section_entry">JDA</p>
-                                <p className="project_entry">G.O.L.I.A.T.H</p>
-                                <p className="client_entry">Tony Stark</p>
-                                <p className="professor_entry">Elizabeth Olsen</p>
-
-                            </div>
-                            <div className="dark_entry">
-                                <p className="checkbox_entry"> _</p>
-                                <p className="team_num_entry">2101</p>
-                                <p className="section_entry">JDA</p>
-                                <p className="project_entry">Helius</p>
-                                <p className="client_entry">Carol Denvers</p>
-                                <p className="professor_entry">Elizabeth Olsen</p>
-                            </div>
-                            <div className="light_entry">
-                                <p className="checkbox_entry"> _</p>
-                                <p className="team_num_entry">2102</p>
-                                <p className="section_entry">JDA</p>
-                                <p className="project_entry">Insight</p>
-                                <p className="client_entry">Peter Parker</p>
-                                <p className="professor_entry">Elizabeth Olsen</p>
-                            </div>
-                            <div className="dark_entry"></div>
-                            <div className="light_entry"></div>
-                            <div className="dark_entry"></div>
-                            <div className="light_entry"></div>
-                            <div className="dark_entry"></div>
-                            <div className="light_entry"></div>
-                            <div className="dark_entry"></div>
-                            <div className="light_entry"></div>
-                            <div className="dark_entry"></div>
-                            <div className="light_entry"></div>
-                            <div className="dark_entry"></div>
+                        <div className="bottom_buttons_group">
+                            <Button variant="contained" onClick={() => {
+                                // TODO: Handle click here
+                                console.log('manage team clicked')
+                            }}
+                            >
+                                Manage Selected Team
+                            </Button>
+                            <Button variant="contained" onClick={() => {
+                                // TODO: Handle click here
+                                console.log('delete team clicked')
+                            }}
+                            >
+                                Delete Selected Teams
+                            </Button>
                         </div>
 
-                        </div>
+                    </div>
                 </div>
-            </body> */
+            </div>
         )
     }
 
