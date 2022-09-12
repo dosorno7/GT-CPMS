f56d21c05f1d9ad7657522cf749413a817a9dfa6
dhenderson49@gatech.edu <dhenderson49@gatech.edu>
2022-09-11 21:01:45 -0400

    Framework for the request access page

--- react-app/src/containers/App/App.tsx
+++ react-app/src/containers/App/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '../App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
@@ -16,8 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React */}
+        {/* </a> */}
       </header>
     </div>
   );
