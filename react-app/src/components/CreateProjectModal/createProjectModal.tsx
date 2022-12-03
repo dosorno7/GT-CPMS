6fb4711af86812513100fe7d7b0eff42ee54e604
lizcooper44 <112188269+lizcooper44@users.noreply.github.com>
2022-12-03 12:35:37 -0500

    Error checking fix & activity status fix (#145)
    * changed team modal to show all fields required instead of errors
    
    * fixed create client modal error checking
    
    * fixed create project modal error checking
    
    Co-authored-by: lizcooper44 <ecooper45@gatech.edu>

--- react-app/src/components/CreateProjectModal/createProjectModal.tsx
+++ react-app/src/components/CreateProjectModal/createProjectModal.tsx
@@ -6,8 +6,10 @@ import FormControl from '@mui/material/FormControl';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import TextField from '@mui/material/TextField'
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 import Modal from '@mui/material/Modal';
+import { FormHelperText, MenuItem } from '@mui/material';
 
 
 const style = {
@@ -26,17 +28,22 @@ export default function CreateProjectModal( {getCreateProjectInfo}: any ) {
     const [open, setOpen] = React.useState(false);
     const [faultyInput, setFaultyInput] = React.useState(false);
     const [disabled, setDisabled] = React.useState(true)
+    const [organization, setOrganization] = React.useState('');
+    const [teamNumber, setTeamNumber] = React.useState('');
+    const [clientName, setClientName] = React.useState('');
+    const [activeStatus, setActiveStatus] = React.useState('');
+    const [section, setSection] = React.useState('')
+    
     const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-
-    const[organization, setOrganization] = React.useState(''); 
-    const[teamNumber, setTeamNumber] = React.useState(''); 
-    const[clientName, setClientName] = React.useState(''); 
-    const[activeStatus, setActiveStatus] = React.useState(''); 
-    const[section, setSection] = React.useState('')
-
-    let section_dict = ["JIA", "JDA", "JDF"];
-    let words = ["Active", "Unassigned", "Completed"]
+    const handleClose = () => {
+        setOrganization('');
+        setTeamNumber('');
+        setSection('');
+        setClientName('');
+        setClientName('');
+        setActiveStatus('');
+        setOpen(false);
+    }
 
     //Event Handlers
     const handleCreateClick = (
@@ -47,35 +54,15 @@ export default function CreateProjectModal( {getCreateProjectInfo}: any ) {
         activeStatus: string) => {
         
         getCreateProjectInfo(teamNumber, section, organization, clientName, activeStatus);
-        if (teamNumber.length == 0 || section.length == 0 || organization.length == 0 || clientName.length == 0|| activeStatus.length == 0) {
-            console.warn("Inputs are wrong");
-            setFaultyInput(true);
-            var val = document.getElementById("modal-modal-error")
-            if (val != null){
-                val.textContent = "Invalid inputs provided";
-            }
-        } else {
-            setFaultyInput(false);
-            handleClose();
-        }
+        handleClose();
     }
 
     const handleTeamNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (!isNaN(Number(event.target.value))) {
-            setTeamNumber(event.target.value);
-        } else {
-            setTeamNumber("");
-        }
+        setTeamNumber(event.target.value);
     }
 
     const handleSectionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        //Add valid section dictionary in db later to replace this code
-        console.log(event.target.value in section_dict)
-        if(section_dict.indexOf(event.target.value) > -1) {
-            setSection(event.target.value);
-        } else {
-            setSection("");
-        }
+        setSection(event.target.value);
     }
 
     const handleorganizationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -83,35 +70,51 @@ export default function CreateProjectModal( {getCreateProjectInfo}: any ) {
     }
 
     const handleClientNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if(event.target.value.length - (event.target.value.indexOf(" ") + 1) > 0  && event.target.value.indexOf(" ") >= 0) {
-            setClientName(event.target.value);
-        } else {
-            setClientName("");
-        }
-        
+        setClientName(event.target.value);        
     }
 
-    const handleStatusChange = (event: React.ChangeEvent<HTMLInputElement>) => {  
-        console.log(words.indexOf(event.target.value))
-        if(words.indexOf(event.target.value) > -1) {
-            setActiveStatus(event.target.value);
-        } else {
-            setActiveStatus("");
-        }
+    const handleStatusChange = (event: SelectChangeEvent) => {  
+        setActiveStatus(event.target.value);
     }
 
 
     React.useEffect(() => {
-        validIn()
+        formValidation()
     });
 
-    function validIn() {
-        if (teamNumber.length == 0 || section.length == 0 || organization.length == 0 || clientName.length == 0|| activeStatus.length == 0) {
-            setDisabled(true);
-        } else {
+    function formValidation() {
+        if (
+            teamNumber != '' && teamNumber.match(/^[0-9]+$/) != null
+            && section != '' && section.toUpperCase() == section 
+            && organization != ''
+            && clientName != ''
+            && activeStatus != '') {
             setDisabled(false);
+        } else {
+            setDisabled(true);
         }
 
+    }
+
+    function sectionHelperText() {
+        if (section === '') {
+            return 'Required *';
+        }
+        if (section.toUpperCase() !== section) {
+            return 'Section code cannot contain lowercase characters';
+        }
+        return '';
+
+    }
+
+    function teamNumberHelperText() {
+        if (teamNumber == '') {
+            return 'Required *';
+        }
+        if (teamNumber.match(/^[0-9]+$/) == null) {
+            return 'Team number must only contain numbers';
+        }
+        return '';
     }
 
     return (
@@ -137,8 +140,7 @@ export default function CreateProjectModal( {getCreateProjectInfo}: any ) {
                             sx={{ m: 1, width: '25ch' }}
                             margin="normal"
                             onChange={handleorganizationChange}
-                            error={organization === ''}
-                            helperText = {organization === '' ? ("This field cannot be empty") : ''}
+                            helperText = {organization === '' ? ("Required *") : ''}
                         />
                         <TextField
                             label="Client Name"
@@ -146,8 +148,7 @@ export default function CreateProjectModal( {getCreateProjectInfo}: any ) {
                             sx={{ m: 1, width: '25ch' }}
                             margin="normal"
                             onChange={handleClientNameChange}
-                            error={clientName === ''}
-                            helperText = {clientName === '' ? ('Please enter a valid (First Last) name') : ''}
+                            helperText = {clientName === '' ? ('Required *') : ''}
 
                         />  
                         <TextField 
@@ -156,8 +157,8 @@ export default function CreateProjectModal( {getCreateProjectInfo}: any ) {
                             sx={{ m: 1, width: '25ch' }} 
                             margin="normal" 
                             onChange={handleTeamNumberChange}
-                            error={teamNumber === ''}
-                            helperText = {teamNumber === '' ? ('Please enter a valid number') : ''}
+                            error={teamNumber.match(/^[0-9]+$/) == null && teamNumber != ''}
+                            helperText={teamNumberHelperText()}
                         />
                         <TextField
                             label="Section"
@@ -165,18 +166,23 @@ export default function CreateProjectModal( {getCreateProjectInfo}: any ) {
                             sx={{ m: 1, width: '25ch' }}
                             margin="normal"
                             onChange={handleSectionChange}
-                            error={section === ''}
-                            helperText = {section === '' ? ('Please enter a valid section') : ''}
+                            error={section.toUpperCase() != section}
+                            helperText = {sectionHelperText()}
                         />
-                        <TextField
-                            label="Activity Status (Active/Not Active)"
-                            id="standard-start-adornment"
-                            sx={{ m: 1, width: '25ch' }}
-                            margin="normal"
-                            onChange={handleStatusChange}
-                            error={activeStatus === ''}
-                            helperText = {activeStatus === '' ? ('Please enter one of: Active, Unassigned, or Completed') : ''}
-                        />
+                        <FormControl style={{ marginBottom: 15, marginLeft: 8, marginTop: 8 }}>
+                            <InputLabel id="select-label">Activity Status</InputLabel>
+                            <Select
+                                value={activeStatus}
+                                onChange={handleStatusChange}
+                                label="Activity Status"
+                                style={{ padding: 10, width: 220, height: 53 }}
+                            >
+                                <MenuItem value="Active">Active</MenuItem>
+                                <MenuItem value="Prospective">Unassigned</MenuItem>
+                                <MenuItem value="Inactive">Completed</MenuItem>
+                            </Select>
+                            <FormHelperText>{activeStatus === '' ? ('Required *') : ''}</FormHelperText>
+                        </FormControl>
                     </Typography>
                     <Button variant="contained" disabled={disabled} onClick={() => { handleCreateClick(teamNumber, section, organization, clientName, activeStatus) }}>
                         Create Project
