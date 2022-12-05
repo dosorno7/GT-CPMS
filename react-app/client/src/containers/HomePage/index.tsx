9476a4e01d3abd89e6c3bda887a1e62c6dc725ed
lizcooper44 <ecooper45@gatech.edu>
2022-12-04 19:09:55 -0500

    fixed tab bar not showing any active tabs

--- react-app/client/src/containers/HomePage/index.tsx
+++ react-app/client/src/containers/HomePage/index.tsx
@@ -19,7 +19,7 @@ class HomePage extends Component {
                         </div>
 
                     </div>
-                    <BasicTabs />
+                    <BasicTabs value={0} />
                 </div>
 
                 <TeamGrid />
